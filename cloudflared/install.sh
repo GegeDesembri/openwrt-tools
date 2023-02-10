ca64635b6af645f04016fb0c3917e20d8fdc1d57
Gege Desembri <62755680+GegeDesembri@users.noreply.github.com>
2023-02-10 19:44:08 +0700

    Update install.sh

--- cloudflared/install.sh
+++ cloudflared/install.sh
@@ -6,7 +6,7 @@ opkg install jq
 
 # Github get data
 get_latest_release() {
-	curl -4 --silent "https://api.github.com/repos/$1/releases/latest" | jq -r .tag_name
+    curl -4 --silent "https://api.github.com/repos/$1/releases/latest" | jq -r .tag_name
 }
 latest_version="$(get_latest_release "cloudflare/cloudflared")"
 wgetlink="https://github.com/cloudflare/cloudflared/releases/download/${latest_version}/cloudflared-linux-arm64"
@@ -20,5 +20,5 @@ wget -qO /etc/init.d/cloudflared "https://raw.githubusercontent.com/GegeDesembri
 chmod +x /etc/init.d/cloudflared
 
 # Enable Cloudflared
-/etc/init.d/cloudflared enable
-/etc/init.d/cloudflared start
+# /etc/init.d/cloudflared enable
+# /etc/init.d/cloudflared start
